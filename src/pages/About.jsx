80d1eff85e92d24f118105fee8e55685dd4a54aa
rcmnrl <ricagayoma@gmail.com>
2023-10-30 05:30:50 +0800

    add about details

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,20 @@
+import React from 'react';
+
 function About(){
     return (
-        <h1> This is About page !</h1>
+        <> 
+        <h4 style={{ fontSize: '24px', color: 'blue' }}>Our Vision</h4>
+        To create a humane community for all cats.
+  
+        <h4 style={{ fontSize: '24px', color: 'blue' }}>Our Mission</h4>
+        To provide education, information, and dialogue that will create a supportive environment, empowering people to help cats in their community.
+  
+        <h4 style={{ fontSize: '24px', color: 'blue' }}>What We Do</h4>
+        We offer a podcast, an informative blog, a grant and mentoring program, online conferences, and networking and sharing opportunities. We participate in cat-related events, and we offer consulting and visioning workshops for organizations.
+      </> 
     )
-}
+  }
+  
 
 
 export default About;
